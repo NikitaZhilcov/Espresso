a196feae0daf4e758526c7c77fd1c1e92f91eaa5
Nikita <zhiltsovna@yandexlyceum.ru>
2021-11-28 13:07:48 +0300

    Commit1

---
+++ main.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*-
+import sqlite3
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        self.con = sqlite3.connect("Сoffee.sqlite")
+
+        res = self.con.cursor().execute("SELECT * FROM Info").fetchall()
+        print(res)
+
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
